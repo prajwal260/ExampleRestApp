c2f251908628bdff37aa92d085f86eb69725cdb8
prajwal1990 <95173929+prajwal1990@users.noreply.github.com>
2022-03-12 17:36:54 +0530

    all files

---
+++ src/test/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
